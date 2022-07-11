ee47811c27cb2c0597a013dc130828dc43dc82e1
josiahfs <josiahfrlswt@gmail.com>
2022-07-12 02:19:38 +0700

    add sex selector

--- bmi/lib/home.dart
+++ bmi/lib/home.dart
@@ -1,8 +1,19 @@
 import 'package:bmi/widgets/button.dart';
 import 'package:flutter/cupertino.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool isSelected = true;
+  String sex = 'Male';
+  void buttonPressed() {
+    setState(() {
+      isSelected = !isSelected;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +22,29 @@ class HomeScreen extends StatelessWidget {
       child: Center(
         child: Column(
           children: [
-            Row(
-              children: [
-                Container(
-                  child: NeuButtons('Male'),
-                )
-              ],
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 36.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    child: NeuButton(
+                      txt: 'MALE',
+                      img: 'assets/male.png',
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    child: NeuButton(
+                      txt: 'FEMALE',
+                      img: 'assets/female.png',
+                    ),
+                  ),
+                ],
+              ),
             )
           ],
         ),
--- bmi/lib/widgets/button.dart
+++ bmi/lib/widgets/button.dart
@@ -1,16 +1,46 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
 
-class NeuButtons extends StatelessWidget {
-  String txt;
-  NeuButtons(this.txt);
+class NeuButton extends StatefulWidget {
+  String txt, img;
+
+  NeuButton({required this.txt, required this.img});
 
   @override
+  State<NeuButton> createState() => _NeuButtonState();
+}
+
+class _NeuButtonState extends State<NeuButton> {
+  bool isPressed = false;
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Container(
-        child: Text(txt),
-        padding: EdgeInsets.all(50),
+    Offset offset = isPressed ? Offset(4, 4) : Offset(6, 6);
+    double blur = isPressed ? 10 : 5;
+
+    return GestureDetector(
+      onTap: (() {
+        setState(() {
+          isPressed = !isPressed;
+        });
+      }),
+      child: AnimatedContainer(
+        width: 136,
+        height: 159,
+        duration: Duration(milliseconds: 170),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(widget.img, width: 59, height: 53),
+            SizedBox(
+              height: 26,
+            ),
+            Text(
+              widget.txt,
+              style: TextStyle(color: Colors.black, fontSize: 24),
+            ),
+          ],
+        ),
+        // padding: EdgeInsets.all(30),
         decoration: BoxDecoration(
             color: Color(0xffE3EDF7),
             borderRadius: BorderRadius.circular(15),
@@ -18,16 +48,18 @@ class NeuButtons extends StatelessWidget {
               // darker bottom right shadow
               BoxShadow(
                 color: Color(0xffAFB9C2),
-                offset: Offset(4, 4),
-                blurRadius: 15,
+                offset: offset,
+                blurRadius: blur,
                 spreadRadius: 1,
+                inset: isPressed,
               ),
               // lighter upper left shadow
               BoxShadow(
                 color: Color(0xffF2FAFF),
-                offset: Offset(-5, -5),
-                blurRadius: 15,
+                offset: -offset,
+                blurRadius: blur,
                 spreadRadius: 1,
+                inset: isPressed,
               ),
             ]),
       ),
